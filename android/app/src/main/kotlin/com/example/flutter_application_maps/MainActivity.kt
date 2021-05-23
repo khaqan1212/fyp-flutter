9a0c4abd642c8f7f83300c138e712ea95a868a08
khaqan1212 <khaqan12@live.com>
2021-05-23 14:46:28 +0500

    My first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_application_maps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_application_maps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,180 @@
+//import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+import "package:google_maps_flutter/google_maps_flutter.dart";
+
+//import 'package:location/location.dart';
+//import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geolocator/geolocator.dart';
+import "package:flutter_application_maps/location.dart";
+
+void main() {
+  runApp(MyApp());
+  //runApp(SettingScreen());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+
+  @override
+  void initState() {}
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<Location> getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    return location;
+  }
+
+  Completer<GoogleMapController> _controller = Completer();
+  static const LatLng _center = const LatLng(45.521563, -122.677433);
+  final Set<Marker> _markers = {};
+  LatLng _lastMapPosition = _center;
+  MapType _currentMapType = MapType.normal;
+  //int _counter = 0;
+
+  //Location _location = Location();
+  //void _incrementCounter() {
+  //  setState(() {
+  //    _counter++;
+  //  });
+  //}
+
+  _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  _onCameraMove(CameraPosition position) {
+    _lastMapPosition = position.target;
+  }
+
+  _onMapTypeButtonPressed() {
+    setState(() {
+      _currentMapType = _currentMapType == MapType.normal
+          ? MapType.satellite
+          : MapType.normal;
+    });
+  }
+
+  _onAddMarkerButtonPressed() async {
+    Location p = await getLocation();
+    double lat = p.latitude;
+    double lon = p.longitude;
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId(_lastMapPosition.toString()),
+          position: LatLng(lat, lon),
+          //position: LatLng(currentLocation.latitude, currentLocation.longitude),
+          infoWindow: InfoWindow(
+            title: 'This is the title',
+            snippet: 'this is the snippet',
+          ),
+          icon: BitmapDescriptor.defaultMarker,
+        ),
+      );
+    });
+  }
+
+  _onSettingsButtonPressed() {
+//    Navigator.push(
+//        context, MaterialPageRoute(builder: (context) => SettingScreen()));
+  }
+
+  Widget button(Function function, IconData icon) {
+    return FloatingActionButton(
+      onPressed: function,
+      materialTapTargetSize: MaterialTapTargetSize.padded,
+      backgroundColor: Colors.blue,
+      child: Icon(
+        icon,
+        size: 36.0,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Stack(children: <Widget>[
+        GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            //target: LatLng(currentLocation.latitude, currentLocation.longitude),
+            zoom: 11.0,
+          ),
+          mapType: _currentMapType,
+          markers: _markers,
+          onCameraMove: _onCameraMove,
+          myLocationEnabled: true,
+        ),
+        Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Align(
+            alignment: Alignment.topRight,
+            child: Column(
+              children: <Widget>[
+                button(_onMapTypeButtonPressed, Icons.map),
+                SizedBox(
+                  height: 16.0,
+                ),
+                button(_onAddMarkerButtonPressed, Icons.add_location),
+                button(_onSettingsButtonPressed, Icons.settings),
+                FloatingActionButton(
+                  onPressed: () {
+//                    Navigator.push(
+//                        context,
+//                        MaterialPageRoute(
+//                            builder: (context) => SettingScreen()));
+                  },
+                  materialTapTargetSize: MaterialTapTargetSize.padded,
+                  backgroundColor: Colors.blue,
+                  child: Icon(
+                    Icons.settings,
+                    size: 36.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.bottomCenter,
+          padding: EdgeInsets.only(bottom: 20),
+          child: Text("Flutter"),
+        )
+      ]),
+      //floatingActionButton: FloatingActionButton(
+      //onPressed: _incrementCounter,
+      // tooltip: 'Increment',
+      // child: Icon(Icons.add),
+      //), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
